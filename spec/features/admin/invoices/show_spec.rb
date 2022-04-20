f85d55869e503a7a4fe8e7ed070a6aff52189d14
craig cardwell <wizeduvcc@gmail.com>
2022-04-20 08:38:51 -0600

    test: add tests to add four attributes to admin invoice show page for item and invoice items

--- spec/features/admin/invoices/show_spec.rb
+++ spec/features/admin/invoices/show_spec.rb
@@ -30,5 +30,36 @@ RSpec.describe 'Admin Invoice Show' do
       expect(page).to have_content('cancelled')
 
     end
+
+    it 'Then I see all of the items on the invoice' do
+      merchant = Merchant.create!(name: 'Brylan')
+      item_1 = merchant.items.create!(name: 'Bottle', unit_price: 10, description: 'H20')
+      item_2 = merchant.items.create!(name: 'Can', unit_price: 5, description: 'Soda')
+      customer = Customer.create!(first_name: "Billy", last_name: "Jonson")
+      invoice_1 = customer.invoices.create(status: "in progress")
+      invoice_2 = customer.invoices.create(status: "in progress")
+      invoice_item_1 = InvoiceItem.create(item_id: item_1.id, invoice_id: invoice_1.id, quantity: 9, unit_price: 10, status: "packaged")
+      invoice_item_2 = InvoiceItem.create(item_id: item_1.id, invoice_id: invoice_2.id, quantity: 10, unit_price: 10, status: "packaged")
+
+      customer_2 = Customer.create!(first_name: "Illy", last_name: "Jonson")
+      invoice_3 = customer_2.invoices.create(status: "in progress")
+      invoice_4 = customer_2.invoices.create(status: "in progress")
+      invoice_item_3 = InvoiceItem.create(item_id: item_2.id, invoice_id: invoice_3.id, quantity: 10, unit_price: 10, status: "packaged")
+      invoice_item_4 = InvoiceItem.create(item_id: item_2.id, invoice_id: invoice_4.id, quantity: 10, unit_price: 10, status: "packaged")
+
+      merchant_2 = Merchant.create!(name: 'Chris')
+      item_3 = merchant_2.items.create!(name: 'Ball', unit_price: 5, description: 'Fun')
+      customer_3 = Customer.create!(first_name: "Illy", last_name: "Jonson")
+      invoice_5 = customer_2.invoices.create(status: "in progress")
+      invoice_item_5 = InvoiceItem.create(item_id: item_3.id, invoice_id: invoice_5.id, quantity: 10, unit_price: 10, status: "packaged")
+
+      visit "/admin/invoices/#{invoice_1.id}"
+
+      expect(page).to have_content("Item Name: Bottle")
+      expect(page).to have_content("Item Quantity: 9")
+      expect(page).to have_content("Item Price: 10")
+      expect(page).to have_content("Invoice Item Status: packaged")
+    end
   end
 end
+
