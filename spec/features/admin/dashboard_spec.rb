e6bc7fc7ca603989d20b1ca2a4fe7971b3350c15
Brylan Gannon <brylan.gannon112@gmail.com>
2022-04-20 13:04:07 -0600

    Test: admin invoices should have invoiced ordered by created_date

--- spec/features/admin/dashboard_spec.rb
+++ spec/features/admin/dashboard_spec.rb
@@ -54,5 +54,41 @@ RSpec.describe 'Admin Dashboard' do
         expect(current_path).to eq("/admin/invoices/#{invoice_1.id}")
       end
     end
+
+    it 'incompleted invoices are in order of created_at' do
+      merchant = Merchant.create!(name: 'Chris')
+      item_1 = merchant.items.create!(name: 'Pencil', unit_price: 500, description: 'Writes things.')
+      item_2 = merchant.items.create!(name: 'Pen', unit_price: 400, description: 'Writes things, but dark.')
+      item_3 = merchant.items.create!(name: 'Marker', unit_price: 400, description: 'Writes things, but dark, and thicc.')
+      item_4 = merchant.items.create!(name: 'Pen', unit_price: 400, description: 'Writes things, but dark.')
+      item_5 = merchant.items.create!(name: 'Marker', unit_price: 400, description: 'Writes things, but dark, and thicc.')
+
+      customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
+      invoice_1 = customer_1.invoices.create!(status: 'completed', created_at: Time.parse("2012-03-27 14:54:09 UTC"))
+      invoice_2 = customer_1.invoices.create!(status: 'completed', created_at: Time.parse("2012-03-22 14:54:09 UTC"))
+      invoice_3 = customer_1.invoices.create!(status: 'completed', created_at: Time.parse("2012-03-26 14:54:09 UTC"))
+      invoice_4 = customer_1.invoices.create!(status: 'completed', created_at: Time.parse("2012-03-24 14:54:09 UTC"))
+      invoice_5 = customer_1.invoices.create!(status: 'completed', created_at: Time.parse("2012-03-28 14:54:09 UTC"))
+      item_1.invoice_items.create!(invoice_id: invoice_1.id, quantity: 3, unit_price: 400, status: 'packaged')
+      item_2.invoice_items.create!(invoice_id: invoice_2.id, quantity: 5, unit_price: 400, status: 'pending')
+      item_3.invoice_items.create!(invoice_id: invoice_3.id, quantity: 5, unit_price: 400, status: 'packaged')
+      item_4.invoice_items.create!(invoice_id: invoice_4.id, quantity: 5, unit_price: 400, status: 'packaged')
+      item_5.invoice_items.create!(invoice_id: invoice_5.id, quantity: 5, unit_price: 400, status: 'shipped')
+
+      invoice_1_date = invoice_1.created_at.strftime("%A, %B %d, %Y")
+      invoice_2_date = invoice_2.created_at.strftime("%A, %B %d, %Y")
+      invoice_3_date = invoice_3.created_at.strftime("%A, %B %d, %Y")
+      invoice_4_date = invoice_4.created_at.strftime("%A, %B %d, %Y")
+      invoice_5_date = invoice_5.created_at.strftime("%A, %B %d, %Y")
+
+      visit '/admin'
+
+      within '#incomplete-invoices' do
+        expect(invoice_2_date).to appear_before(invoice_4_date)
+        expect(invoice_4_date).to appear_before(invoice_3_date)
+        expect(invoice_3_date).to appear_before(invoice_1_date)
+        expect(invoice_1_date).to appear_before(invoice_5_date)
+      end
+    end
   end
 end
