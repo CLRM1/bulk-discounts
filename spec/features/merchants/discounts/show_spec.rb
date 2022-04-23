4d9411c21a1548cb9cff15a084d632068fb248fa
Chris Romero <chrisleeromero1@gmail.com>
2022-04-23 10:44:17 -0600

    feat: bulk discount show page displays percentage discount and quantity threshold

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe 'bulk discount index' do
+RSpec.describe 'bulk discount show' do
 
   before(:each) do
     @merchant = Merchant.create!(name: 'Chris')
@@ -64,41 +64,23 @@ RSpec.describe 'bulk discount index' do
     @invoice_6.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
   end
 
-  it 'allows the user to create a new discount' do
-    visit "/merchants/#{@merchant.id}/bulk_discounts"
-    click_link "Create New Discount"
-    expect(current_path).to eq("/merchants/#{@merchant.id}/bulk_discounts/new")
-    fill_in 'percentage_discount', with: 20
-    fill_in 'quantity_threshold', with: 10
-    click_button 'Submit'
-    expect(current_path).to eq("/merchants/#{@merchant.id}/bulk_discounts")
-  end
 
-  it 'allows the user to delete a discount' do
+  # Merchant Bulk Discount Show
+  #
+  # As a merchant
+  # When I visit my bulk discount show page
+  # Then I see the bulk discount's quantity threshold and percentage discount
+  it 'displays the bulk discount quantity threshold and percentage discount' do
     discount_1 = @merchant.bulk_discounts.create(percentage_discount: 10, quantity_threshold: 20)
-    discount_2 = @merchant.bulk_discounts.create(percentage_discount: 20, quantity_threshold: 10)
-    discount_3 = @merchant.bulk_discounts.create(percentage_discount: 30, quantity_threshold: 5)
+    discount_2 = @merchant.bulk_discounts.create(percentage_discount: 25, quantity_threshold: 15)
+    visit "/merchants/#{@merchant.id}/bulk_discounts/#{discount_1.id}"
 
-    visit "/merchants/#{@merchant.id}/bulk_discounts"
-    within "#discount-#{discount_1.id}" do
-      expect(page).to have_content(discount_1.id)
-      click_link 'Delete'
-    end
-    expect(current_path).to eq("/merchants/#{@merchant.id}/bulk_discounts")
-    expect(page).to_not have_content(discount_1.id)
+    expect(page).to have_content(discount_1.id)
+    expect(page).to have_content(discount_1.percentage_discount)
+    expect(page).to have_content(discount_1.quantity_threshold)
 
-    within "#discount-#{discount_2.id}" do
-      expect(page).to have_content(discount_2.id)
-      click_link 'Delete'
-    end
-    expect(current_path).to eq("/merchants/#{@merchant.id}/bulk_discounts")
     expect(page).to_not have_content(discount_2.id)
-
-    within "#discount-#{discount_3.id}" do
-      expect(page).to have_content(discount_3.id)
-      click_link 'Delete'
-    end
-    expect(current_path).to eq("/merchants/#{@merchant.id}/bulk_discounts")
-    expect(page).to_not have_content(discount_3.id)
+    expect(page).to_not have_content(discount_2.percentage_discount)
+    expect(page).to_not have_content(discount_2.quantity_threshold)
   end
 end
