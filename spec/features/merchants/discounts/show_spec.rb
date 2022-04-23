8a8cf0d3948ab854b6bfe0e811a304e728669437
Chris Romero <chrisleeromero1@gmail.com>
2022-04-23 11:01:21 -0600

    test: bulk discount show page should allow user to edit discount

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -5,71 +5,8 @@ RSpec.describe 'bulk discount show' do
   before(:each) do
     @merchant = Merchant.create!(name: 'Chris')
     @merchant_2 = Merchant.create!(name: 'Sophie')
-    @item_1 = @merchant.items.create!(name: 'Pencil', unit_price: 500, description: 'Writes things.')
-    @item_2 = @merchant.items.create!(name: 'Pen', unit_price: 400, description: 'Writes things, but dark.')
-    @item_3 = @merchant.items.create!(name: 'Marker', unit_price: 400, description: 'Writes things, but dark, and thicc.')
-
-    @customer_1 = Customer.create!(first_name: 'Joey', last_name: 'Ondricka')
-    @invoice_1 = @customer_1.invoices.create!(status: 'completed')
-    @invoice_7 = @customer_1.invoices.create!(status: 'completed')
-    @item_1.invoice_items.create!(invoice_id: @invoice_1.id, quantity: 3, unit_price: 400, status: 'packaged',
-                                                                                       created_at: Time.parse("2012-03-27 14:54:09 UTC"))
-    @item_2.invoice_items.create!(invoice_id: @invoice_7.id, quantity: 5, unit_price: 400, status: 'packaged',
-                                                                                       created_at: Time.parse("2012-03-28 14:54:09 UTC"))
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249632', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249631', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249633', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'success')
-    @invoice_1.transactions.create!(credit_card_number: '4654405418249635', result: 'success')
-
-    @customer_2 = Customer.create!(first_name: 'Osinski', last_name: 'Cecelia')
-    @invoice_2 = @customer_2.invoices.create!(status: 'completed')
-    @item_1.invoice_items.create!(invoice_id: @invoice_2.id, quantity: 3, unit_price: 400, status: 'packaged',
-                                                                                       created_at: Time.parse("2012-03-29 14:54:09 UTC"))
-    @invoice_2.transactions.create!(credit_card_number: '5654405418249632', result: 'success')
-    @invoice_2.transactions.create!(credit_card_number: '5654405418249631', result: 'success')
-    @invoice_2.transactions.create!(credit_card_number: '5654405418249633', result: 'success')
-    @invoice_2.transactions.create!(credit_card_number: '5654405418249633', result: 'success')
-    @invoice_2.transactions.create!(credit_card_number: '5654405418249633', result: 'success')
-
-    @customer_3 = Customer.create!(first_name: 'Toy', last_name: 'Mariah')
-    @invoice_3 = @customer_3.invoices.create!(status: 'completed')
-    @item_1.invoice_items.create!(invoice_id: @invoice_3.id, quantity: 3, unit_price: 400, status: 'packaged',
-                                                                                       created_at: Time.parse("2012-03-30 14:54:09 UTC"))
-    @invoice_3.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_3.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
-    @invoice_3.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
-    @invoice_3.transactions.create!(credit_card_number: '6654405418249631', result: 'success')
-
-    @customer_4 = Customer.create!(first_name: 'Joy', last_name: 'Braun')
-    @invoice_4 = @customer_4.invoices.create!(status: 'completed')
-    @item_1.invoice_items.create!(invoice_id: @invoice_4.id, quantity: 3, unit_price: 400, status: 'packaged',
-                                                                                       created_at: Time.parse("2012-04-01 14:54:09 UTC"))
-    @invoice_4.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_4.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_4.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-
-    @customer_5 = Customer.create!(first_name: 'Mark', last_name: 'Brains')
-    @invoice_5 = @customer_5.invoices.create!(status: 'completed')
-    @item_1.invoice_items.create!(invoice_id: @invoice_5.id, quantity: 3, unit_price: 400, status: 'packaged',
-                                                                                       created_at: Time.parse("2012-04-02 14:54:09 UTC"))
-    @invoice_5.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-    @invoice_5.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
-
-    @customer_6 = Customer.create!(first_name: 'Smark', last_name: 'Mrains')
-    @invoice_6 = @customer_6.invoices.create!(status: 'completed')
-    @item_1.invoice_items.create!(invoice_id: @invoice_6.id, quantity: 3, unit_price: 400, status: 'packaged',
-                                                                                       created_at: Time.parse("2012-04-03 14:54:09 UTC"))
-    @invoice_6.transactions.create!(credit_card_number: '6654405418249632', result: 'success')
   end
 
-
-  # Merchant Bulk Discount Show
-  #
-  # As a merchant
-  # When I visit my bulk discount show page
-  # Then I see the bulk discount's quantity threshold and percentage discount
   it 'displays the bulk discount quantity threshold and percentage discount' do
     discount_1 = @merchant.bulk_discounts.create(percentage_discount: 10, quantity_threshold: 20)
     discount_2 = @merchant.bulk_discounts.create(percentage_discount: 25, quantity_threshold: 15)
@@ -82,5 +19,25 @@ RSpec.describe 'bulk discount show' do
     expect(page).to_not have_content(discount_2.id)
     expect(page).to_not have_content(discount_2.percentage_discount)
     expect(page).to_not have_content(discount_2.quantity_threshold)
+  end
+
+  it 'allows the user to edit the discount' do
+    discount_1 = @merchant.bulk_discounts.create(percentage_discount: 10, quantity_threshold: 20)
+    discount_2 = @merchant.bulk_discounts.create(percentage_discount: 25, quantity_threshold: 15)
+    visit "/merchants/#{@merchant.id}/bulk_discounts/#{discount_1.id}"
+
+    click_link "Edit"
+    expect(current_path).to eq("/merchants/#{@merchant.id}/bulk_discounts/#{discount_1.id}/edit")
+    expect(page).to have_content(discount_1.percentage_discount)
+    expect(page).to have_content(discount_1.quantity_threshold)
+    fill_in 'Percentage Discount', with: 22
+    fill_in 'Quantity Threshold', with: 2
+    click_button 'Submit'
+    
+    expect(current_path).to eq("/merchants/#{@merchant.id}/bulk_discounts/#{discount_1.id}")
+    expect(page).to_not have_content(10)
+    expect(page).to_not have_content(20)
+    expect(page).to have_content(22)
+    expect(page).to have_content(2)
   end
 end
