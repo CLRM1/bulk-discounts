a215655430570b2ea5e1d923d6b47ef1f4ea1820
Chris Romero <chrisleeromero1@gmail.com>
2022-04-24 12:12:45 -0600

    feat: model test working with ruby code for discounted_revenue method

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -19,6 +19,29 @@ class Invoice < ApplicationRecord
 
   def total_discounted_revenue
     gross_revenue = total_revenue
+     # a  = select.("merchants.*, bulk_discounts.* FROM bulk_discounts JOIN merchants ON merchants.id = bulk_discounts.merchant_id")
     # require 'pry'; binding.pry
+    customer_id = self.customer_id
+    customer = Customer.find(customer_id)
+    invoices = customer.invoices
+    merchant = invoices.first.merchant
+    discounts = merchant.first.bulk_discounts
+
+    if discounts.first.percentage_discount != nil && discounts.first.quantity_threshold != nil
+      percentage_discount = discounts.first.percentage_discount
+      quantity_threshold = discounts.first.quantity_threshold
+      new_price = 0
+    end
+
+    if invoice_items.first.quantity >= quantity_threshold
+      discounted_amount = (((invoice_items.first.unit_price) * 0.20) * invoice_items.first.quantity)
+    end
+    new_price = gross_revenue - discounted_amount
+
+    # select *. discounts from customer.invoices.merchant.bulk_discounts
+    # joins customers and bulk discounts
+    # where
+    # group by
+
   end
 end
